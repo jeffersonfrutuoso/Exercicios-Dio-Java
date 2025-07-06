40a43156fc9750f284a80bd1f4be742f46670d03
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-06 16:10:54 -0300

    teste

--- src/exercicios/exercicio17.java
+++ src/exercicios/exercicio17.java
@@ -9,9 +9,12 @@ public class exercicio17 {
 //        Imprima na tela as duas temperaturas.
 //        Fórmula: C = (5 * ( F-32) / 9)
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Digite uma Temperatura em Fahrenheit");
         int tempFahrenheit = scanner.nextInt();
+
         int calc = (5* (tempFahrenheit - 32) / 9 );
+
         System.out.println("temperatura em Fahrenheit: " + tempFahrenheit+ "F");
         System.out.println("temperatura em graus celsius: " + calc+ "C");
     }
